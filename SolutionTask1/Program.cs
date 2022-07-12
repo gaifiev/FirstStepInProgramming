cdb797da7beba15e37f3c725da5995b62fd6d4a1
gaifiev <v.gaifiev@mail.ru>
2022-07-12 10:55:57 +0300

    add SolutionTask1 and .gitignore

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+
+if (inputLineOne != null && inputLineTwo != null)
+{
+    int inputNumberOne = int.Parse(inputLineOne);
+    int inputNumberTwo = int.Parse(inputLineTwo);
+
+    // if (inputNumberOne == inputNumberTwo * inputLineTwo)
+    // {
+    //     Console.WriteLine("Yes");
+    // }
+    // else
+    // {
+    //     Console.WriteLine("No");
+    // }
+    if (inputNumberTwo == Math.Sqrt(inputNumberOne))
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
