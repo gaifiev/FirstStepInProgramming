73e1fb0a4a120fb1ac6ce1d3d1801c94864c9a78
gaifiev <v.gaifiev@mail.ru>
2022-07-12 19:19:26 +0300

    add solution task 4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,19 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    char[] M = inputLine.ToCharArray();
+    Console.WriteLine(M[2]);
+    // int inputNumber = int.Parse(inputLine);
+    // int outNumber = inputNumber % 10;
+    // Console.WriteLine(outNumber);
+}
+
+// string? inputLine = Console.ReadLine();
+
+// if (inputLine != null)
+// {
+//     string lastNumber = inputLine.Substring(2, 1);
+//     Console.WriteLine(lastNumber);
+// }
+
