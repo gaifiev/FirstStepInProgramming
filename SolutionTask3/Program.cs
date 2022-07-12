8be810894d58c23939d2789fd0ee976868159c56
gaifiev <v.gaifiev@mail.ru>
2022-07-12 18:59:54 +0300

    add solution task 3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,14 @@
+﻿string? inputLineOne = Console.ReadLine();
+if (inputLineOne != null)
+{
+    int inputNumber = int.Parse(inputLineOne);
+    int startNumber = 1;
+    string lineOutput = "0";
+    while (startNumber <= inputNumber)
+    {
+        lineOutput = startNumber * (-1) + ", " + lineOutput + ", " + startNumber;
+        startNumber++;
+    }
+    //lineOutput = lineOutput + inputNumber;
+    Console.WriteLine(lineOutput);
+}
