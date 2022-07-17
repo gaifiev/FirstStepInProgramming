de16f4ee2060cc314bbc1a9559972d185320e994
gaifiev <v.gaifiev@mail.ru>
2022-07-17 17:18:39 +0300

    add solution task 11

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+//первый вариант решения
+void variant1()
+{
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine("Число:" + number);
+    int numberSecondDigit = number / 100;
+    int numberThirdDigit = number % 10;
+    Console.WriteLine("Число после удаления второй цифры: " + numberSecondDigit + numberThirdDigit);
+}
+
+
+//Второй вариант решения
+void variant2()
+{
+    int number = new Random().Next(100, 1000);
+    Console.Write("Рандом число это: ");
+    Console.WriteLine(number);
+    string stringNum = number.ToString();
+    Console.Write("modified num is : :");
+    Console.Write(stringNum[0]);
+    Console.WriteLine(stringNum[2]);
+}
+
+//Третий вариант решения
+void variant3()
+{
+    int number = new Random().Next(100, 1000);
+    Console.Write("Рандом число это: ");
+    Console.WriteLine(number);
+
+    int num1 = number / 100;
+    int num2 = number % 10;
+
+    Console.Write("modified num is : :");
+    Console.WriteLine(num1 * 10 + num2);
+}
+
+variant1();
+variant2();
+variant3();
