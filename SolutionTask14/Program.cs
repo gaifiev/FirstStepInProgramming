5511dd055bfa83e313893001b56409ebb02de903
gaifiev <v.gaifiev@mail.ru>
2022-07-17 19:08:07 +0300

    add solution for task 14

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// Например: 14 - НЕТ
+// 46 - НЕТ
+// 161 - Да
+
+string reminderSearcher(string? str)
+{
+    if (str != null)
+    {
+        try
+        {
+            string answer;
+            int num = int.Parse(str);
+            if (((num % 7) == 0) && ((num % 23) == 0))
+            {
+                answer = "It's multiple for 7 and 23";
+            }
+            else
+            {
+                answer = "It's not multiple for 7 and 23";
+            }
+            return answer;
+        }
+        catch (Exception e)
+        {
+            return "Возникло исключение: " + e.Message;
+        }
+    }
+    return "";
+}
+Console.WriteLine("Input number: ");
+string? inputOne = Console.ReadLine();
+Console.WriteLine(reminderSearcher(inputOne));
