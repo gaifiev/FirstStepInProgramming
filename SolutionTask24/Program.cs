b1ac862f4b803763aae9d852185007a5314d1b6a
gaifiev <v.gaifiev@mail.ru>
2022-08-04 15:53:51 +0300

    add solution for task 24

---
+++ SolutionTask24/Program.cs
@@ -0,0 +1,38 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А. 
+// Например: 
+// 7 = 28
+// 4 = 10
+// 8 = 36
+
+Console.Write("Введите число: ");
+
+int inputNumber = int.Parse(Console.ReadLine());
+int t;
+
+void VariantSimple()
+{
+    int sumOfNumbers = 0;
+    for (int i = 1; i <= inputNumber; i++)
+    {
+        sumOfNumbers += i;
+        // sumOfNumbers = sumOfNumbers + i;
+    }
+    Console.WriteLine("Сумма чисел от 1 до " + inputNumber + " = " + sumOfNumbers);
+}
+
+void VariantGauss()
+{
+    int sumOfNumbers = 0;
+
+    sumOfNumbers = ((1 + inputNumber) * inputNumber) / 2;
+
+    Console.WriteLine("Сумма чисел от 1 до " + inputNumber + " = " + sumOfNumbers);
+}
+
+t = Environment.TickCount;
+VariantSimple();
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);
+
+t = Environment.TickCount;
+VariantGauss();
+Console.WriteLine("Gauss time: {0} ms", Environment.TickCount - t);
