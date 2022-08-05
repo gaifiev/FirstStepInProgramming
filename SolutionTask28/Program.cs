791a0dcd07e57147f95ecdfdd93bd10d94cbd14b
gaifiev <v.gaifiev@mail.ru>
2022-08-05 14:27:26 +0300

    solution for task 28

---
+++ SolutionTask28/Program.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+// Например:
+// 4 = 24
+// 5 = 120
+
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+int inputNum = int.Parse(inputLine);
+int t;
+
+t = Environment.TickCount;
+Console.WriteLine(sumNums(inputNum));
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);
+
+t = Environment.TickCount;
+Console.WriteLine(mulRec(inputNum));
+Console.WriteLine("Recurs time: {0} ms", Environment.TickCount - t);
+
+int sumNums(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= inputNum; i++)
+    {
+        // sum *= i;
+        sum = sum * i;
+    }
+    return sum;
+}
+
+int mulRec(int num)
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * mulRec(num - 1);
+    }
+}
