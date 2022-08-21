ba293d03f1daaa10c35cb59aeb13010e95f051f5
gaifiev <v.gaifiev@mail.ru>
2022-08-22 01:51:56 +0300

    add 51-62 solution task

---
+++ SolutionTask54/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+int[,] FillTwoDimArray(int countRow, int countColumn) //метод для заполнения двумерного массива
+{
+    System.Random numberSyntezator = new System.Random();
+    int i = 0;
+    int j = 0;
+    int[,] outArray = new int[countRow, countColumn];
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSyntezator.Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0;
+    int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            Console.Write(inputArray[i, j] + "\t");
+            Console.ResetColor();
+            Thread.Sleep(10);
+            j++;
+        }
+        Console.WriteLine();
+        i++;
+    }
+}
+
+void Filter(int[,] inputArray) // метод убывания
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            for (int b = 0; b < inputArray.GetLength(1) - 1; b++)
+            {
+                if (inputArray[i, b] < inputArray[i, b + 1])
+                {
+                    int temp = inputArray[i, b + 1];
+                    inputArray[i, b + 1] = inputArray[i, b];
+                    inputArray[i, b] = temp;
+                }
+            }
+        }
+    }
+}
+int[,] twoDimArray = FillTwoDimArray(5, 5);
+Filter(twoDimArray);
+PrintTwoDimArray(twoDimArray);
---
+++ SolutionTask55/Program.cs
@@ -0,0 +1,105 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+// void Read()
+// {
+//     Console.WriteLine("Введите число А: ");
+//     int a = int.Parse(Console.ReadLine()!);
+//     Console.WriteLine("Вы ввели число " + a);
+
+//     Console.WriteLine("Введите число B: ");
+//     int b = int.Parse(Console.ReadLine()!);
+//     Console.WriteLine("Вы ввели число " + b);
+
+//     Console.WriteLine("Введите число C: ");
+//     int c = int.Parse(Console.ReadLine()!);
+//     Console.WriteLine("Вы ввели число " + c);
+// }
+
+// int ReadCoord(string line)
+// {
+//     Console.WriteLine(line);
+//     int buf = int.Parse(Console.ReadLine());
+//     Console.WriteLine("Вы ввели число " + buf);
+//     return buf;
+// }
+
+// int a = ReadCoord("Введите число А: ");
+// int b = ReadCoord("Введите число B: ");
+// int c = ReadCoord("Введите число C: ");
+
+
+// метод для заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    System.Random numberSyntezator = new System.Random();
+    int i = 0; int j = 0;
+    int[,] outArray = new int[countRow, countColumn];
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSyntezator.Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i, j] + "\t");
+            j++;
+        }
+        Console.WriteLine();
+        i++;
+    }
+}
+
+// Метод меняет местами первую ипоследнюю строчку
+// Принимает двумерный массив
+// Возвращает модефицированный входной массив
+int[,] RotateTwoDimArray(int[,] inputArray)
+{
+    int bufferElement = 0;
+    int i = 0;
+    int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = i;
+        while (j < inputArray.GetLength(0))
+        {
+            //Блок ротации элементов в столбце
+            bufferElement = inputArray[i, j];
+            inputArray[i, j] = inputArray[j, i];
+            inputArray[j, i] = bufferElement;
+            j++;
+        }
+        i++;
+    }
+    return inputArray;
+}
+
+
+int[,] twoDimArray = FillTwoDimArray(4, 4);
+PrintTwoDimArray(twoDimArray);
+Console.WriteLine();
+
+if (twoDimArray.GetLength(0) != twoDimArray.GetLength(1))
+{
+    Console.WriteLine("Данный массив перевернуть нельзя!");
+}
+else
+{
+    //----------------------------------------------------------
+    PrintTwoDimArray(RotateTwoDimArray(twoDimArray));
+    //----------------------------------------------------------
+}
