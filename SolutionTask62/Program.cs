f2f5367e604bedb8208b65fe21402978e197bc6c
gaifiev <v.gaifiev@mail.ru>
2022-08-23 07:56:26 +0300

    add 62 solution task

--- SolutionTask62/Program.cs
+++ SolutionTask62/Program.cs
@@ -4,3 +4,57 @@
 // 12 13 14 5
 // 11 16 15 6
 // 10 9 8 7
+
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+int n = 4;
+int[,] sqareMatrix = new int[n, n];
+int[,] Spiral(int[,] squareMatrix)//метод для заполнения дву массива
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+    while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+    {
+        sqareMatrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return sqareMatrix;
+}
+
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0;
+    int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            if (inputArray[i, j] / 10 <= 0)
+                Console.Write($" {inputArray[i, j]} ");
+            else Console.Write($"{inputArray[i, j]} ");
+            Console.ResetColor();
+            Thread.Sleep(10);
+            j++;
+        }
+        Console.WriteLine();
+        i++;
+    }
+}
+
+Spiral(sqareMatrix);
+PrintTwoDimArray(sqareMatrix);
