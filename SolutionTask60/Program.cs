e33090bd5867232bb4c6d89412fd849687e894e0
gaifiev <v.gaifiev@mail.ru>
2022-08-23 04:49:52 +0300

    add 60 solution task

--- SolutionTask60/Program.cs
+++ SolutionTask60/Program.cs
@@ -2,3 +2,74 @@
 // Массив размером 2 x 2 x 2
 // 12(0,0,0) 22(0,0,1)
 // 45(1,0,0) 53(1,0,1)
+
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+void FillTwoDimArray(int[,,] array3D)//метод для заполнения трехмерного массива
+{
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+void PrintTwoDimArray(int[,,] inputArray)
+{
+    int i = 0;
+    int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.Write($"X({i}) Y({j}) ");
+            for (int k = 0; k < inputArray.GetLength(2); k++)
+            {
+                Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+                Console.Write($"Z({k})={inputArray[i, j, k]}; ");
+                Console.ResetColor();
+                Thread.Sleep(10);
+            }
+            j++;
+        }
+        Console.WriteLine();
+        i++;
+    }
+}
+
+int[,,] array3D = new int[2, 2, 2];
+FillTwoDimArray(array3D);
+PrintTwoDimArray(array3D);
